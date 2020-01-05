eb76dd029557f29ca2ff9fdc7d730bdc7cb84833
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-04 21:19:14 -0800

    set framework for creating new html doc

---
+++ html.js
@@ -0,0 +1,17 @@
+function createNewDoc(data, themeColor) {
+    const {
+      avatar_url: imgUrl,
+      name,
+      location,
+      html_url: githubUrl,
+      blog: blogUrl,
+      bio,
+      public_repos: repos,
+      followers,
+      public_gists: stars,
+      following
+    } = data;
+    return ``;
+  }
+  
+  module.exports = createNewDoc;
