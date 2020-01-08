16da495b40c4cec98cf9adb6fadcc56a7e8bfd80
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-07 22:46:52 -0800

    html fix

--- html.js
+++ html.js
@@ -40,129 +40,98 @@ const colors = {
     } = data;
     return `<!DOCTYPE html>
     <html lang="en">
-       <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-          <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-          <title>Document</title>
-          <style>
-              @page {
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <title>Document</title>
+        <!--------------StyleBEGIN------------->
+        <style>
+             h1 {
+                font-size: 48px;
+             }
+             h2 {
+                font-size: 40px;
+             }
+             h3 {
+                font-size: 32px;
+             }
+             h4 {
+                font-size: 24px;
+             }
+             h5 {
+                font-size: 20px;
+             }
+             h6 {
+                font-size: 20px;
+             }
+             h1, h2, h3, h4, h5, h6 {
+                font-family: Arial, Helvetica, sans-serif;
                 margin: 0;
-              }
-             *,
-             *::after,
-             *::before {
-             box-sizing: border-box;
-             }
-    
-             html, body {
-             padding: 0;
-             margin: 0;
-             }
-    
-             html, body, .wrapper {
-             height: 100%;
              }
     
              .wrapper {
-             background-color: ${colors[themeColor].wrapperBackground};
-             padding-top: 100px;
+                background-color: ${colors[themeColor].wrapperBackground};
+                padding-top: 100px;
              }
     
              body {
-             background-color: white;
-             -webkit-print-color-adjust: exact !important;
-             font-family: 'Cabin', sans-serif;
+                background-color: white;
+                font-family: Arial, Helvetica, sans-serif;
              }
     
              main {
-             background-color: #E9EDEE;
-             height: auto;
-             padding-top: 30px;
+                background-color: #E9EDEE;
+                height: auto;
+                padding-top: 30px;
              }
     
-             h1, h2, h3, h4, h5, h6 {
-             font-family: 'BioRhyme', serif;
-             margin: 0;
+             html, body, .wrapper {
+                height: 100%;
              }
     
-             h1 {
-             font-size: 3em;
-             }
-             h2 {
-             font-size: 2.5em;
-             }
-             h3 {
-             font-size: 2em;
-             }
-             h4 {
-             font-size: 1.5em;
-             }
-             h5 {
-             font-size: 1.3em;
-             }
-             h6 {
-             font-size: 1.2em;
+             .picBox {
+                 background-color: ${colors[themeColor].headerBackground};
+                 color: ${colors[themeColor].headerColor};
+                 padding: 10px;
+                 width: 95%;
+                 border-radius: 5px;
+                 position: relative;
+                 margin: 0 auto;
+                 margin-bottom: -50px;
+                 display: flex;
+                 justify-content: center;
+                 flex-wrap: wrap;
              }
     
-             .photo-header {
-             position: relative;
-             margin: 0 auto;
-             margin-bottom: -50px;
-             display: flex;
-             justify-content: center;
-             flex-wrap: wrap;
-             background-color: ${colors[themeColor].headerBackground};
-             color: ${colors[themeColor].headerColor};
-             padding: 10px;
-             width: 95%;
-             border-radius: 6px;
+             .picBox img {
+                width: 220px;
+                height: 220px;
+                border-radius: 50%;
+                object-fit: contain;
+                margin-top: -75px;
+                border: 5px solid ${colors[themeColor].photoBorderColor};
+                box-shadow: 4px 1px 20px 4px black;
              }
     
-             .photo-header img {
-             width: 250px;
-             height: 250px;
-             border-radius: 50%;
-             object-fit: cover;
-             margin-top: -75px;
-             border: 6px solid ${colors[themeColor].photoBorderColor};
-             box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-             }
-    
-             .photo-header h1, .photo-header h2 {
-             width: 100%;
-             text-align: center;
-             }
-    
-             .photo-header h1 {
-             margin-top: 10px;
-             }
-    
-             .links-nav {
-             width: 100%;
-             text-align: center;
-             padding: 20px 0;
-             font-size: 1.1em;
-             }
-    
-             .nav-link {
-             display: inline-block;
-             margin: 5px 10px;
-             }
-    
-             .workExp-date {
-             font-style: italic;
-             font-size: .7em;
-             text-align: right;
-             margin-top: 10px;
+             .picBox h1, .picBox h2 {
+                width: 100%;
+                text-align: center;
              }
     
              .container {
              padding: 50px;
              padding-left: 100px;
              padding-right: 100px;
+             }
+    
+             .card {
+               padding: 20px;
+               border-radius: 6px;
+               background-color: ${colors[themeColor].headerBackground};
+               color: ${colors[themeColor].headerColor};
+               margin: 20px;
              }
     
              .row {
@@ -174,85 +143,89 @@ const colors = {
              }
              
              .col {
-             flex: 1;
-             text-align: center;
-             }
-    
-             .card {
-               padding: 20px;
-               border-radius: 6px;
-               background-color: ${colors[themeColor].headerBackground};
-               color: ${colors[themeColor].headerColor};
-               margin: 20px;
+                flex: 1;
+                text-align: center;
              }
     
              a, a:hover {
-             text-decoration: none;
-             color: inherit;
-             font-weight: bold;
+                text-decoration: none;
+                color: inherit;
+                font-weight: bold;
              }
     
-             @media print { 
-              body { 
-                zoom: .75; 
-              } 
+             .links-nav {
+                 width: 100%;
+                 text-align: center;
+                 padding: 20px 0;
+                 font-size: 1.1em;
              }
-          </style>
-            <body>
+    
+             .nav-link {
+                display: inline-block;
+                margin: 5px 10px;
+             }
+             
+             
+        </style>
+        <!--------------StyleEND------------->
+    </head>
+    <body>
         <div class="wrapper">
-          <div class="photo-header">
-            <img src="${imgUrl}" alt="" />
-            <h1>Hi!</h1>
-            <h1>My name is ${name}</h1>
-            <h4>Currently @ Trilogy Education Services</h4>
-            <div class="links-nav">
-              <a class="nav-link" href="#" target="_blank"
-                ><i class="fas fa-location-arrow"></i> ${location}</a
-              >
-              <a class="nav-link" href="${githubUrl}" target="_blank"
-                ><i class="fab fa-github-alt"></i> GitHub</a
-              >
-              <a class="nav-link" href="${blogUrl}" target="_blank"
-                ><i class="fas fa-rss"></i> Blog</a
-              >
+            <div class="picBox">
+              <img src="${imgUrl}" alt="profile_pic" />
+              <h1>Hi!</h1>
+              <h1>My name is ${name}</h1>
+              <h4>Currently at ...</h4>
+              <div class="links-nav">
+                <a class="nav-link" href="#" target="_blank"
+                  ><i class="fas fa-location-arrow"></i> ${location}</a
+                >
+                <a class="nav-link" href="${githubUrl}" target="_blank"
+                  ><i class="fab fa-github-alt"></i> GitHub</a
+                >
+                <a class="nav-link" href="${blogUrl}" target="_blank"
+                  ><i class="fas fa-rss"></i> Blog</a
+                >
+              </div>
             </div>
+            <main>
+              <div class="container">
+                <div class="row">
+                  <div class="col"><h2>${bio}</h2></div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <div class="card">
+                      <h3>Public Repositories</h3>
+                      <h4>${repos}</h4>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="card">
+                      <h3>Followers</h3>
+                      <h4>${followers}</h4>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <div class="card">
+                      <h3>GitHub Stars</h3>
+                      <h4>${stars}</h4>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="card">
+                      <h3>Following</h3>
+                      <h4>${following}</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </main>
           </div>
-          <main>
-            <div class="container">
-              <div class="row">
-                <div class="col"><h2>${bio}</h2></div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="card">
-                    <h3>Public Repositories</h3>
-                    <h4>${repos}</h4>
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="card">
-                    <h3>Followers</h3>
-                    <h4>${followers}</h4>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="card">
-                    <h3>GitHub Stars</h3>
-                    <h4>${stars}</h4>
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="card">
-                    <h3>Following</h3>
-                    <h4>${following}</h4>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </main>
-        </div>
-      </body>`;
+        
+    </body>
+    </html>`;
     }
   module.exports = generateHtml;
